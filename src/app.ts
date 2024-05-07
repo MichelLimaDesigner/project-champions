e5042548f1d47ee66cf62990bf64a26228888ddd
michellimadesigner <michellimadesigner@outlook.com>
2024-05-07 20:15:03 -0300

    feat: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
+import cors from "cors";
 import router from "./routes";
 
 function createApp() {
   const app = express();
   app.use(json());
+  app.use(cors());
   app.use("/api", router);
 
   return app;
