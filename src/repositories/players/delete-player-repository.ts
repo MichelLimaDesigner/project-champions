37cb5bfa34bcfc22f4be7d35392714805f62d4ff
michellimadesigner <michellimadesigner@outlook.com>
2024-05-07 19:18:57 -0300

    fix: add conditional to delete player

--- src/repositories/players/delete-player-repository.ts
+++ src/repositories/players/delete-player-repository.ts
@@ -1,7 +1,12 @@
 import { database } from "./players-database";
 
-export const deleteOnePlayer = async (id: number) => {
+export const deleteOnePlayer = async (id: number): Promise<boolean> => {
   const index = database.findIndex((player) => player.id === id);
 
-  if (index !== -1) database.splice(index, 1);
+  if (index !== -1) {
+    database.splice(index, 1);
+    return true;
+  }
+
+  return false;
 };
--- src/services/players/delete-player-service.ts
+++ src/services/players/delete-player-service.ts
@@ -1,15 +1,16 @@
 import { HttpResponse } from "../../models/http-response-model";
 import { deleteOnePlayer } from "../../repositories/players/delete-player-repository";
-import { ok } from "../../utils/http-helper";
+import { badRequest, ok } from "../../utils/http-helper";
 
 export const deletePlayerService = async (
   id: number
 ): Promise<HttpResponse> => {
   let response = null;
 
-  await deleteOnePlayer(id);
+  const isDeleted = await deleteOnePlayer(id);
 
-  response = ok({});
+  if (isDeleted) response = await ok({});
+  else response = await badRequest();
 
   return response;
 };
